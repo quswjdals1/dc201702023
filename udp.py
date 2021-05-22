0c1ba56d5f257fd54c8b9efa883cba1e1da7f7f8
quswjdals1 <bjmonly1@naver.com>
2021-05-22 14:08:14 +0900

    updateabc

--- udp.py
+++ udp.py
@@ -1,1 +1,15 @@
-print('d')
+import socket
+
+ip='0.0.0.0'
+port = 8001
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind((ip,port))
+
+print("udp server started")
+while True:
+    data,addr = sock.recvfrom(1024)
+    print("received message",data.decode('utf-8'), "from",addr)
+    currentTime = " " + time.ctime(time.time())+"\r\n"
+    data = data+currentTime.encode('ascii')
+    sock.sendto(data,addr)
